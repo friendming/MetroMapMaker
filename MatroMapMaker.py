f69b8bdfb68eb947894986fde94650140fe5409f
friendming <qm760681623@163.com>
2019-09-01 23:29:54 +0800

    update

--- MatroMapMaker.py
+++ MatroMapMaker.py
@@ -1,19 +1,42 @@
 import turtle           #paint
 import tkinter          #gui
 import pyautogui as pag #gui
-import pyhook
-import os               #os库
+#import os              #os库
 
 
 def initWindow():
-    turtle.pensize()
+    turtle.setup(800,600,0,0)
+    turtle.up()
+    turtle.goto(0,0)
     return
 
 def getMouseXY_pag():
     return pag.position()
 
-def getMouseXY_ph():
-    return
+class sMetroWay:
+    '直线way 宽,颜色,初始x,初始y,结束x,结束y'
+    def __init__(self,width,color,stx,sty,endx,endy):
+        self.width = width
+        self.color = color
+        self.stx = stx
+        self.sty = sty
+        self.endx = endx 
+        self.endy = endy
+        self.width = width
+
+
+    def showWay(self):
+        turtle.color(self.color)
+        turtle.pensize(self.width)
+        turtle.goto(self.stx, self.sty)
+        turtle.down()
+        turtle.goto(self.endx, self.endy)
+        turtle.up()
+
+
 
 #MainLoop
-getMouseXY_pag()
+mouse_XY = getMouseXY_pag()
+way1 = sMetroWay(50,"red",10,10,10,20)
+way1.showWay()
+turtle.done()
