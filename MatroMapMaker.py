9b5812992a1381e13ae4b6f3ce1fb2aef727bb3e
friendming <qm760681623@163.com>
2019-09-01 16:29:01 +0800

    q

---
+++ MatroMapMaker.py
@@ -0,0 +1,19 @@
+import turtle           #paint
+import tkinter          #gui
+import pyautogui as pag #gui
+import pyhook
+import os               #os库
+
+
+def initWindow():
+    turtle.pensize()
+    return
+
+def getMouseXY_pag():
+    return pag.position()
+
+def getMouseXY_ph():
+    return
+
+#MainLoop
+getMouseXY_pag()
